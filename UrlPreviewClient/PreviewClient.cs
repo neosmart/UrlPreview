fbe59a8d4d54e5ee5f8d7b4a8eca2b5b2d200d6b
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-07-10 13:20:36 -0500

    Added command line test client

---
+++ UrlPreviewClient/PreviewClient.cs
@@ -0,0 +1,31 @@
+﻿using NeoSmart.UrlPreview;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlPreviewClient
+{
+    class PreviewClient
+    {
+        public async Task Start()
+        {
+            while (true)
+            {
+                Console.Write("Preview URL: ");
+                var url = Console.ReadLine();
+
+                await Preview(url);
+            }
+        }
+
+        private async Task Preview(string url)
+        {
+            var preview = new UrlPreview(url);
+            await preview.GetPreviewAsync();
+            var serialized = JsonConvert.SerializeObject(preview, Formatting.Indented);
+            Console.WriteLine(serialized);
+        }
+    }
+}
---
+++ UrlPreviewClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace UrlPreviewClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new PreviewClient();
+            client.Start().GetAwaiter().GetResult();
+        }
+    }
+}
