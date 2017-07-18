3c8dd6c5f93393c2c29a3a3babca07267047f139
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-07-18 18:32:34 -0500

    Basing the decision of infinite redirects on the entire request
    We were previously only using the presence of an already-seen URI as the
    "Location" target in a response from the server as reason enough to
    consider the redirect a cyclic/infinite redirect and abort.
    
    This is apparently not good enough because some people at some big sites
    like eBay thought it was a good reason to write code that bounces users
    back and forth between the same URLs, but changing the cookie field
    (amongst others) and thereby changing the actions and ultimately serving
    up a response.

--- UrlPreview/HttpClientExtensions.cs
+++ UrlPreview/HttpClientExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -32,9 +33,15 @@ namespace NeoSmart.UrlPreview
 
         public static async Task<HttpResponseMessage> GetAsyncRedirect(this HttpClient client, Uri uri, CancellationToken? cancel = null)
         {
-            HashSet<string> visited = new HashSet<string>() { uri.ToString() };
+            //we used to use only the URI as our cylic request detector but that hasn't worked out too well
+            //you'd be surprised at the number of top 50 sites that redirect repeatedly but take different actions
+            //based on the cookie and other request headers.
+            //Now we deduplicate based off of state; i.e. the entire request header that is made
+            var visited = new HashSet<UniqueRequest>();
 
             var response = await client.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead, cancel ?? CancellationToken.None);
+            Debug.WriteLine("Initial request: {0}", uri);
+            visited.Add(UniqueRequest.From(response.RequestMessage));
             while (((int)response.StatusCode) >= 300 && (int)response.StatusCode < 400)
             {
                 response.Dispose();
@@ -46,7 +53,8 @@ namespace NeoSmart.UrlPreview
                         throw new InvalidRedirectException(uri.ToString(), redirect.ToString());
                     }
                 }
-                if (!visited.Add(redirect.ToString()))
+                Debug.WriteLine("Redirecting to {0}", redirect);
+                if (!visited.Add(UniqueRequest.From(response.RequestMessage)))
                 {
                     throw new InvalidRedirectException($"Infinite redirection encountered loading URI {uri}");
                 }
---
+++ UrlPreview/UniqueRequest.cs
@@ -0,0 +1,68 @@
+﻿using NeoSmart.Hashing.XXHash;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace NeoSmart.UrlPreview
+{
+    /// <summary>
+    /// An HTTP request is uniquely identified by more than just the URL, it's the entire request.
+    /// Just because a server keeps 301ing you to the same request over and over again, it does not 
+    /// actually mean there is an infinite loop - it could be ammending the request you make until
+    /// some pre-condition is met at which time the request will go through, as stupid as this 
+    /// approach may be. *cough* eBay *cough*
+    /// </summary>
+    struct UniqueRequest
+    {
+        /// <summary>
+        /// This is a URI because URI encoding can differ but the URL can ultimately be the same
+        /// </summary>
+        public Uri RequestUri;
+        public HttpMethod Method;
+        public SortedSet<KeyValuePair<string, IEnumerable<string>>> Headers;
+
+        public override int GetHashCode()
+        {
+            var xxState = XXHash.CreateState32();
+            XXHash.UpdateState32(xxState, ToBytes(RequestUri.ToString()));
+            XXHash.UpdateState32(xxState, ToBytes(Method.Method));
+            
+            foreach (var kv in Headers)
+            {
+                //case-insensitive keys
+                XXHash.UpdateState32(xxState, ToBytes(kv.Key.ToLowerInvariant()));
+                foreach (var headerValue in kv.Value)
+                {
+                    //case-sensitive values?
+                    XXHash.UpdateState32(xxState, ToBytes(headerValue));
+                }
+            }
+
+            return (int) XXHash.DigestState32(xxState);
+        }
+
+        public byte[] ToBytes(string s)
+        {
+            return Encoding.UTF8.GetBytes(s);
+        }
+
+        public static UniqueRequest From(HttpRequestMessage request)
+        {
+            return new UniqueRequest
+            {
+                RequestUri = request.RequestUri,
+                Method = request.Method,
+                Headers = new SortedSet<KeyValuePair<string, IEnumerable<string>>>(request.Headers, new HeaderCollectionComparer()),
+            };
+        }
+
+        class HeaderCollectionComparer : IComparer<KeyValuePair<string, IEnumerable<string>>>
+        {
+            public int Compare(KeyValuePair<string, IEnumerable<string>> x, KeyValuePair<string, IEnumerable<string>> y)
+            {
+                return StringComparer.CurrentCultureIgnoreCase.Compare(x.Key, y.Key);
+            }
+        }
+    }
+}
