f3b17bbeb04ea6c583bebd2440a21392c9ff4213
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2021-09-02 12:58:05 -0500

    Fix selection of Amazon preview image

--- UrlPreview/Loaders/AmazonLoader.cs
+++ UrlPreview/Loaders/AmazonLoader.cs
@@ -12,13 +12,17 @@ namespace NeoSmart.UrlPreview.Loaders
         public override Task<string> ExtractThumbnailAsync()
         {
             // As of 2021, the main image for an Amazon product listing URL is "span[data-action=main-image-click] img"
-            var mainImage = Html.Document.SelectSingleNode("span[@data-action='main-image-click']//img[@href]");
+            var mainImage = Html.Document.SelectSingleNode("//span[@data-action='main-image-click']//img[@src]");
             if (mainImage is null)
             {
-                return base.ExtractThumbnailAsync();
+                mainImage = Html.Document.SelectSingleNode("//img[@id='landingImage'][@src]");
+                if (mainImage is null)
+                {
+                    return base.ExtractThumbnailAsync();
+                }
             }
 
-            var url = mainImage.GetAttributeValue("href", null);
+            var url = mainImage.GetAttributeValue("src", null);
             return Task.FromResult(url);
         }
     }
